469c95f460b36545805f40091dd5365ca518e05f
squatch-in <jvwyatt7@gmail.com>
2024-08-01 20:10:41 -0500

    bug fixes

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-
-
-
 //randomly choose between one of the values
-let humanScore = 0;
-let computerScore = 0;
 
-let randomNum = Math.floor(Math.random() * (3 - 0)) + 1;
+var humanScore = 0;
+var computerScore = 0;
+let humanSelection = getHumanChoice();
+let computerSelection = numConvert(); 
 
-    
-
-function numConvert() {           //assign the random number to back to a word
+function numConvert() {           
     let result;
+    let randomNum = Math.floor(Math.random() * (3 - 0)) + 1;
     if (randomNum === 1) {
         result = "rock";
     }else if (randomNum === 2) {
@@ -20,29 +17,28 @@ function numConvert() {           //assign the random number to back to a word
     }
     return result;
 }
-console.log(numConvert());
 
-let human = prompt("rock, paper, scissors");
+//assign the random number to back to a word
 
+
+//get the user to type there response
 function getHumanChoice() {
-                                            //get the user to type there response
+    let result;
+   human = prompt("rock, paper, scissors");
     if (human.toLowerCase() === "rock"){
         alert("you chose rock");
+        result = "rock";
     }else if (human.toLowerCase() === "paper"){
         alert("you chose paper");
+        result = "paper";
     }else if (human.toLowerCase() === "scissors") {
         alert("you chose scissors");
+        result = "scissors";
     }
-    return human;
+    return result;
 }
 
-//console.log(getHumanChoice());
-
-const humanSelection = getHumanChoice();
-const computerSelection = numConvert();
-
-
- //compare the output of the 2 functions
+//compare the output of the 2 functions
 function playRound(humanSelection, computerSelection) { 
     let result;
     if (humanSelection == "rock" && computerSelection == "rock") {
@@ -67,24 +63,31 @@ function playRound(humanSelection, computerSelection) {
     return result;
 }
 
-console.log(playRound(humanSelection, computerSelection));
+// console.log(playRound(humanSelection, computerSelection));
 
-var result = playRound(humanSelection, computerSelection);
 
+// var result = playRound(humanSelection, computerSelection);
 function playerScore(result){                   //decides between who won and who lost
     if (result == "you win") {
         humanScore++;
+        return humanScore;
     }else (result == "you lose")
-    computerScore++;
+        computerScore++;
+        return computerScore;
 }
-playerScore();
-console.log(humanScore);                //returns scores
+
+console.log(humanScore);                
 console.log(computerScore);
+//returns scores
 // console.log(playerScore(result));
 
-function playGame() {                                   //function that will play the game for 5 times
+//function that will play the game for 5 times
+function playGame() {                                   
     for(let i = 1; i < 5; i++){
-        whoWon = playerScore(result);
+        numConvert();
+        getHumanChoice();
+        playRound(humanSelection, computerSelection);
+        playerScore(result);   
     }
 }
 playGame();
