c138349b01555d7c2d0daee3ab233b3e87c51900
squatch-in <jvwyatt7@gmail.com>
2024-08-03 21:53:49 -0500

    a few bugs still, im moving on for now

--- script.js
+++ script.js
@@ -5,26 +5,12 @@ var computerScore = 0;
 let humanSelection = getHumanChoice();
 let computerSelection = numConvert(); 
 
-function numConvert() {           
-    let result;
-    let randomNum = Math.floor(Math.random() * (3 - 0)) + 1;
-    if (randomNum === 1) {
-        result = "rock";
-    }else if (randomNum === 2) {
-        result = "paper";
-    }else if (randomNum === 3) {
-        result = "scissors";
-    }
-    return result;
-}
-
-//assign the random number to back to a word
 
 
 //get the user to type there response
 function getHumanChoice() {
     let result;
-   human = prompt("rock, paper, scissors");
+    human = prompt("rock, paper, scissors");
     if (human.toLowerCase() === "rock"){
         alert("you chose rock");
         result = "rock";
@@ -33,6 +19,20 @@ function getHumanChoice() {
         result = "paper";
     }else if (human.toLowerCase() === "scissors") {
         alert("you chose scissors");
+        result = "scissors";
+    }
+    return result;
+}
+
+//assign the random number to back to a word
+function numConvert() {           
+    let result;
+    let randomNum = Math.floor(Math.random() * (3 - 0)) + 1;
+    if (randomNum === 1) {
+        result = "rock";
+    }else if (randomNum === 2) {
+        result = "paper";
+    }else if (randomNum === 3) {
         result = "scissors";
     }
     return result;
@@ -63,31 +63,31 @@ function playRound(humanSelection, computerSelection) {
     return result;
 }
 
-// console.log(playRound(humanSelection, computerSelection));
+let whoWon = playRound(humanSelection, computerSelection);
+ //decides between who won and who lost
 
-
-// var result = playRound(humanSelection, computerSelection);
-function playerScore(result){                   //decides between who won and who lost
-    if (result == "you win") {
+function playerScore(whoWon){  
+    let result;                
+    if (whoWon == "you win") {
         humanScore++;
-        return humanScore;
-    }else (result == "you lose")
+    }else if (whoWon == "you lose") {
         computerScore++;
-        return computerScore;
+    }else if (whoWon == "tie"){
+        computerScore;
+        humanScore;
+    }
+    return result;
 }
 
-console.log(humanScore);                
-console.log(computerScore);
-//returns scores
-// console.log(playerScore(result));
 
-//function that will play the game for 5 times
+// function that will play the game for 5 times 
+
 function playGame() {                                   
-    for(let i = 1; i < 5; i++){
-        numConvert();
-        getHumanChoice();
-        playRound(humanSelection, computerSelection);
-        playerScore(result);   
+    for(let i = 0; i < 5; i++){
+        playerScore(whoWon); 
+        playRound(getHumanChoice(), numConvert());
     }
 }
 playGame();
+
+alert("your score was " + humanScore + " the cpu score was " + computerScore);
